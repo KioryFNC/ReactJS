269db11ac40e7fc0d5ac9de5a50a2e1678d5ea3c
yuri <yuri.barros.luz.familia@gmail.com>
2024-11-28 14:59:59 -0300

    comunication between components

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import styles from './Comment.module.css'
 import { Avatar } from './avatar'
 
 // a baixo estou usando a desestruturação em ingles destructuring, e pegando apenas oque vou usar no caso author, publishedAt e content, e onde eu quero chamar ele eu só chamo entre {}, mas pode fazer sem desestruturar que é na função chamar somente o (props) ao inves de ({author, ..., ...}) e na hora que for chamar a props tem que colocar props.author ou o nome que vc quer chamar tipo assim  <p>{props.content}</p> se fizer a desestruturação é só colocar direto  <p>{content}</p>
-export function Comment({content}) {
+export function Comment({content, onDeleteComment}) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return(
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/KioryFNC.png" alt="" />
@@ -16,7 +20,7 @@ export function Comment({content}) {
               <time title="11 de Maio ás 08:13h" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24}/>
             </button>
           </header>
