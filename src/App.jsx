212e52fd172f24f1b0a7e1a440a94ecd818565ef
yuri <yuri.barros.luz.familia@gmail.com>
2024-12-10 16:37:08 -0300

    adds

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,13 @@ const posts = [
     id: 2,
     author: {
       avatarUrl: 'https://github.com/diego3g.png',
-      name:'Yuri Barros',
+      name:'Diego Fernandes',
       role: 'CTO  @Rocketseat'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
-
       //  <a href="">#novoprojeto</a>{' '}
       //  <a href="">#nlw</a>{' '}
       //  <a href="">#rocketseat</a>
